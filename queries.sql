7975ec1fdb90e0e796758d732b25a3936dbce2df
Nivedita <sachniv1023@gmail.com>
2021-08-11 11:11:10 -0500

    changes done

--- queries.sql
+++ queries.sql
@@ -1,4 +1,7 @@
-/*#find out the customer who's age is between 30 to 60 and their experience and education_level.
+/*#1.List customer's experience and education_level where customer's age is greater than 30..
+#Explanation 
+#In my select statement  I have selected cust_id,age,experience,education_level 
+#from customer_details and then further used a where clause for getting the results based on the specified condition..
 
 /*select cust_id,age,experience,education_level
 from customer_details
@@ -6,6 +9,11 @@ WHERE age >30
 order by age desc;*/
 
 /*2.#find out customer who's income is more than 180.
+
+#Explanation 
+#In my select statement I have selected age, education_level,income then further joined it to customer_income table 
+#using joins. Inner join are basically used to get records which are common in both tables and further joined it to 
+#income and then used where clause for getting customers where income is greater than 180.
 
 /*select  age, education_level, income
 from customer_details
@@ -16,6 +24,11 @@ on income.income_id=customer_income.income_id
 where income>180;*/
 
 /*3.#find out customer who r interedsted in personal loan and having family size=3, and higher educated. 
+
+#Explanation
+#In my select statement I have selected education_level,family,personal_loan from customer_details and then used a where clause for filtering the records bsed on 
+#condition.
+
 /*Select education_level,family,personal_loan
 from customer_details 
 where
@@ -33,8 +46,12 @@ where income>100;*/
 
 /*#5.find out the customer who's mortgage is greater than 0 and ccavg is greater than 1.
 
-select  age, education_level,cust_id, family, ccavg, mortgage
+#Explanation
+#In my select statement I have selected age, education_level,cust_id, family, ccavg, mortgage from customer_details 
+#and then joined with mortgage table on mortgage_id and then used where clause for filtering results.
+
+/*select  age, education_level,cust_id, family, ccavg, mortgage
 from customer_details
 inner join mortgage
 on customer_details.mortgage_id=mortgage.mortgage_id
-where mortgage>'0' and ccavg>1 ;
+where mortgage>'0' and ccavg>1 ;*/
